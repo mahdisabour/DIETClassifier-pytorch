ec847c0df33e77d227722d5ea336ca28394ecd1f
mahdisabour <msbaour.official@gmail.com>
2025-04-15 06:36:30 +0000

    feat: add train file and some change in dataset

---
+++ train.py
@@ -0,0 +1,11 @@
+from src.models.wrapper import DIETClassifierWrapper
+
+config_file = "src/config.yml"
+wrapper = DIETClassifierWrapper(config=config_file)
+
+#predict
+wrapper.predict(["How to check attendance?"])
+
+#train
+#after training, wrapper will load best model automatically
+wrapper.train_model(save_folder="test_model")
